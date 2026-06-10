8990604bbc641aba05c26f66c1d712b03fa5133a
Rolando Valle <rolandovalle0428@gmail.com>
2026-06-10 14:22:07 +0800

    update

--- app/robots.ts
+++ app/robots.ts
@@ -13,6 +13,6 @@ export default function robots(): MetadataRoute.Robots {
         allow: "/",
       },
     ],
-    sitemap: "https://arra-and-robert.weddinginvitationrsvp.com/sitemap.xml",
+    sitemap: "https://karla-kaye-and-felix-jr-invitation.weddinginvitationrsvp.com/sitemap.xml",
   }
 }
